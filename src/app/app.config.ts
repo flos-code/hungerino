79508fa4275b5ec48f867251ec9aacfbde3ad01c
flos-code <148456982+flos-code@users.noreply.github.com>
2024-03-02 11:19:55 +0100

    initial commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,11 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"hungerino-46fa6","appId":"1:941655761150:web:d761993e1fed61047c05af","storageBucket":"hungerino-46fa6.appspot.com","locationId":"europe-west","apiKey":"AIzaSyANkY4Gf3Z-_uvW9r4h17FZNr9SBkkmxiQ","authDomain":"hungerino-46fa6.firebaseapp.com","messagingSenderId":"941655761150"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
+};
